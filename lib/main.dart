74e84775a33ad35534bdbcf99b7cad273082a39d
Curiklo <45988993+Curiklo@users.noreply.github.com>
2019-12-28 14:18:47 +0900

    add days counter & tab function

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,108 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+  home:CountDays(),
+));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class CountDays extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _CountDaysState createState() => _CountDaysState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _CountDaysState extends State<CountDays> {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  var startDay = DateTime(2015,DateTime.october,1);
+  var today = DateTime.now();
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void initState(){
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+      length: 7,
+      child: Scaffold(
+        appBar: AppBar(
+          title:Text('Count Days'),
+          centerTitle: true,
+          backgroundColor: Colors.orange[300],
+        ),
+        body:TabBarView(
+          // each tab content
+          children: [
+            Center(
+            child: Text(
+            '${today.difference(startDay).inDays}',
+            style: TextStyle(
+              fontSize: 50.0,
+              letterSpacing: 2.0,
+              color: Colors.grey,
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Icon(Icons.battery_std),
+            Icon(Icons.loyalty),
+            Icon(Icons.grade),
+            Icon(Icons.android,color: Colors.green,size: 100.0,),
+            Icon(Icons.store),
+            Icon(Icons.widgets)
           ],
         ),
+        /**/
+        bottomNavigationBar: SafeArea(
+          child: TabBar(
+            // tab option
+            isScrollable: true,
+            unselectedLabelColor: Colors.black.withOpacity(0.3),
+            unselectedLabelStyle: TextStyle(fontSize: 12.0),
+            labelColor: Colors.black,
+            labelStyle: TextStyle(fontSize: 16.0),
+            indicatorColor: Colors.orange,
+            indicatorWeight: 2.0,
+            // content of tab
+            tabs: [
+              Tab(
+                child: Icon(
+                    Icons.favorite,
+                    color: Colors.pink,
+                ),
+              ),
+              Tab(
+                child: Icon(
+                    Icons.add,
+                ),
+              ),
+              Tab(
+                child: Icon(
+                Icons.sentiment_very_satisfied,
+                ),
+              ),
+              Tab(
+                child: Icon(
+                  Icons.photo_album,
+                ),
+              ),
+              Tab(
+                child: Icon(
+                  Icons.explore,
+                ),
+              ),
+              Tab(
+                child: Icon(
+                  Icons.info,
+                ),
+              ),
+              Tab(
+                child: Icon(
+                  Icons.import_contacts,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
